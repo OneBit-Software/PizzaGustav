3d3cb8f399edac5b1fdd1fbc9627f059837a41ce
Rafael Strehler <rafael.strehler@outlook.com>
2018-05-05 10:24:09 +0200

    some path conventions fixed

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import Drawer from './src/components/navigation';
+import Drawer from './src/components/Navigation';
 
 export default class App extends React.Component{
   render(){
